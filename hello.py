199f997f2c5efb03054490a246837338a8c9dbb9
AmolG10 <97290320+AmolG10@users.noreply.github.com>
2022-01-07 17:28:46 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+import logging
+
+Build='Build......'
+print(Build)
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
---
+++ python_run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+"C:\Program Files\Python310\python.exe" "D:\vitas_proj\vitas_poc_1\scripts\hello.py"
+
+pause
