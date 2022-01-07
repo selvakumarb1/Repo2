b3ddeb0236abb42d9971e3c404618cd1de52c4fa
AmolG10 <97290320+AmolG10@users.noreply.github.com>
2022-01-07 17:27:42 +0530

    Add files via upload

---
+++ checkout.bat
@@ -0,0 +1,14 @@
+@eco off
+set path_for_repo_fd=%~dp0
+cd  %path_for_repo_fd%
+git --version    rem git cmd
+dir  
+git status
+git log
+git log --pretty=oneline 
+git show
+git tag
+git branch -a
+git checkout tags/V1.1
+git checkout SubBranch_Sample
+
