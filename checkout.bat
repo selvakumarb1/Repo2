bb9164e13968a9379793056d84a39f41c5009ae8
AmolG10 <97290320+AmolG10@users.noreply.github.com>
2022-01-18 11:50:38 +0530

    Update checkout.bat

--- checkout.bat
+++ checkout.bat
@@ -1,4 +1,5 @@
 @echo off
+set selected_CommitID=%1
 set path_for_repo_fd=%~dp0
 cd  %path_for_repo_fd%
 git --version    rem git cmd
@@ -7,6 +8,7 @@ git status
 git log --pretty=oneline 
 git tag
 git branch -a
-git checkout tags/V1.1
-git checkout SubBranch_Sample
+git checkout %selected_CommitID%
+rem git checkout tags/V1.1
+rem git checkout SubBranch_Sample
 
